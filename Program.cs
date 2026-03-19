7faac69c4a4e9061d6a4a0dd0986f1065212fee5
Khanh048 <voquockhanh.nlt@gmail.com>
2026-03-19 12:28:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Bai3
+{
+    static void Main()
+    {
+        Console.WriteLine("Ho va ten: Vo Quoc Khanh");
+        Console.WriteLine("MSV: 2415053122121");
+        Console.WriteLine("Lop: 225LTC#01");
+        Console.WriteLine();
+
+        List<int> numbers = new List<int>();
+        Console.Write("Nhap so luong phan tu: ");
+        int count = int.Parse(Console.ReadLine());
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"Nhap phan tu {i + 1}: ");
+            
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+        int max = numbers[0];
+        foreach (int n in numbers)
+            if (n > max) max = n;
+        Console.WriteLine($"So lon nhat: {max}");
+        Console.ReadKey();
+    }
+}
