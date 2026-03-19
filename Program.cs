4d4f95a60b6a2e5726844c8d0cbd4544c4081cce
Khanh048 <voquockhanh.nlt@gmail.com>
2026-03-19 12:43:21 +0700

    Bai13

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Student
+{
+    public int Id;
+    public string Name;
+}
+
+class Bai13
+{
+    static void Main()
+    {
+        Console.WriteLine("Ho va ten: Vo Quoc Khanh");
+        Console.WriteLine("MSV: 2415053122121");
+        Console.WriteLine("Lop: 225LTC#01");
+        Console.WriteLine();
+
+        List<Student> students = new List<Student> {
+            new Student { Id = 1, Name = "Nguyen Van An" },
+            new Student { Id = 2, Name = "Tran Thi Binh" },
+            new Student { Id = 3, Name = "Le Van Chau" }
+        };
+        Console.WriteLine($"Tong so sinh vien: {students.Count}");
+        Console.ReadKey();
+    }
+}
